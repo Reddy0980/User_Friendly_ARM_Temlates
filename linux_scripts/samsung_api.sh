caa88e3a708eb2cb21785112f1b5984c76faacac
Adarsh Anand <adarshanshu7@gmail.com>
2021-09-14 13:14:04 +0530

    Update samsung_api.sh

--- linux_scripts/samsung_api.sh
+++ linux_scripts/samsung_api.sh
@@ -6,7 +6,7 @@ adddate() {
     done
 }
 
-ip=public_ip
+ip=ip_address
 url="https://$ip/api/serverInfo"
 
 echo "Cluster IP: $ip" >> adddate >> samsung.log
@@ -19,6 +19,7 @@ while [ $N -lt 1 ]
 do
    rm samsung.json
    echo "curl -X GET --header 'Accept: application/json' -k $url" >> adddate >> samsung.log
+   curl -X GET --header 'Accept: application/json' -k $url
    curl -X GET --header 'Accept: application/json' -k $url >> samsung.json
    grep clusterState samsung.json >> adddate >> samsung.log
    if grep -o ACTIVE samsung.json
