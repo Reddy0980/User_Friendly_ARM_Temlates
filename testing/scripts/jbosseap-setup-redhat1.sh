937d9c5f89e606a40f8e06b53e9890fddf3c00a7
Adarsh Anand <adarshanshu7@gmail.com>
2021-10-11 17:38:59 +0530

    Update jbosseap-setup-redhat1.sh

--- testing/scripts/jbosseap-setup-redhat1.sh
+++ testing/scripts/jbosseap-setup-redhat1.sh
@@ -13,7 +13,7 @@ openport() {
 }
 
 echo "Red Hat JBoss EAP Cluster Intallation Start " | log; flag=${PIPESTATUS[0]}
-/bin/date +%H:%M:%S  | log; flag=${PIPESTATUS[0]}
+/bin/date +%H:%M:%S | log; flag=${PIPESTATUS[0]}
 
 export EAP_LAUNCH_CONFIG="/opt/rh/eap7/root/usr/share/wildfly/bin/standalone.conf"
 echo 'export EAP_RPM_CONF_STANDALONE="/etc/opt/rh/eap7/wildfly/eap7-standalone.conf"' >> ~/.bash_profile
@@ -45,20 +45,18 @@ JBOSS_EAP_USER=$9
 JBOSS_EAP_PASSWORD=${10}
 RHSM_USER=${11}
 RHSM_PASSWORD=${12}
-EAP_POOL=${13}
+RHSM_POOL=${13}  # have not passed rhel pool
 STORAGE_ACCOUNT_NAME=${14}
 CONTAINER_NAME=${15}
 STORAGE_ACCESS_KEY=$(echo "${16}" | openssl enc -d -base64)
-RHEL_POOL=${17} # kept at the end because it is possible that customer won't provide this.
+RHEL_POOL=${17}
 IP_ADDR=$(hostname -I)
 
 echo "JBoss EAP admin user: " ${JBOSS_EAP_USER} | log; flag=${PIPESTATUS[0]}
-echo "JBoss EAP on RHEL version you selected : JBoss-EAP7.3-on-RHEL8.0" | log; flag=${PIPESTATUS[0]}
-echo "Storage Account Name: " ${STORAGE_ACCOUNT_NAME} | log; flag=${PIPESTATUS[0]}
+echo "JBoss EAP on RHEL version you selected : JBoss-EAP7.3-on-RHEL8.0"| log; flag=${PIPESTATUS[0]}
+echo "Storage Account Name: " ${STORAGE_ACCOUNT_NAME} | log; flag=${PIPESTATUS[0]} 
 echo "Storage Container Name: " ${CONTAINER_NAME} | log; flag=${PIPESTATUS[0]}
 echo "RHSM_USER: " ${RHSM_USER} | log; flag=${PIPESTATUS[0]}
-
-echo "Folder where script is executing ${pwd}" | log; flag=${PIPESTATUS[0]}
 
 ####################### Configuring firewall for ports
 echo "Configure firewall for ports 8080, 9990, 45700, 7600" | log; flag=${PIPESTATUS[0]}
@@ -70,10 +68,8 @@ openport 8080
 openport 9990
 openport 45700
 openport 7600
-
 echo "firewall-cmd --reload" | log; flag=${PIPESTATUS[0]}
 sudo firewall-cmd  --reload  | log; flag=${PIPESTATUS[0]}
-
 echo "iptables-save" | log; flag=${PIPESTATUS[0]}
 sudo iptables-save   | log; flag=${PIPESTATUS[0]}
 ####################### 
@@ -81,23 +77,19 @@ sudo iptables-save   | log; flag=${PIPESTATUS[0]}
 echo "Initial JBoss EAP setup" | log; flag=${PIPESTATUS[0]}
 ####################### Register to subscription Manager
 echo "Register subscription manager" | log; flag=${PIPESTATUS[0]}
-echo "subscription-manager register --username $RHSM_USER --password RHSM_PASSWORD" | log; flag=${PIPESTATUS[0]}
+echo "subscription-manager register --username RHSM_USER --password RHSM_PASSWORD" | log; flag=${PIPESTATUS[0]}
 subscription-manager register --username $RHSM_USER --password $RHSM_PASSWORD --force | log; flag=${PIPESTATUS[0]}
 if [ $flag != 0 ] ; then echo  "ERROR! Red Hat Manager Registration Failed" >&2 log; exit $flag;  fi
 #######################
-
-sleep 20
-
 ####################### Attach EAP Pool
 echo "Subscribing the system to get access to JBoss EAP repos" | log; flag=${PIPESTATUS[0]}
 echo "subscription-manager attach --pool=EAP_POOL" | log; flag=${PIPESTATUS[0]}
-subscription-manager attach --pool=${EAP_POOL}  | log; flag=${PIPESTATUS[0]}
+subscription-manager attach --pool=${RHSM_POOL} | log; flag=${PIPESTATUS[0]}
 if [ $flag != 0 ] ; then echo  "ERROR! Pool Attach for JBoss EAP Failed" >&2 log; exit $flag;  fi
 #######################
-
 ####################### Attach RHEL Pool
 echo "Attaching Pool ID for RHEL OS" | log; flag=${PIPESTATUS[0]}
-if [ "$EAP_POOL" != "$RHEL_POOL" ]; then
+if [ "$RHSM_POOL" != "$RHEL_POOL" ]; then
     echo "subscription-manager attach --pool=RHEL_POOL" | log; flag=${PIPESTATUS[0]}
     subscription-manager attach --pool=${RHEL_POOL}  | log; flag=${PIPESTATUS[0]}
     if [ $flag != 0 ] ; then echo  "ERROR! Pool Attach for RHEL Failed" >&2 log; exit $flag;  fi
@@ -106,11 +98,10 @@ else
 fi
 #######################
 
-
 ####################### Install openjdk: is it needed? it should be installed with eap7.3
 echo "Install openjdk, wget, git, unzip, vim" | log; flag=${PIPESTATUS[0]}
-echo "sudo yum install java-1.8.0-openjdk wget unzip vim git -y" | log; flag=${PIPESTATUS[0]}
-sudo yum install wget unzip vim git -y | log; flag=${PIPESTATUS[0]}
+echo "sudo yum install java-1.8.0-openjdk wget unzip vim git -y" | log; flag=${PIPESTATUS[0]}install.log
+sudo yum install wget unzip vim git -y | log; flag=${PIPESTATUS[0]} #java-1.8.0-openjdk
 ####################### 
 
 
@@ -121,8 +112,8 @@ if [ $flag != 0 ] ; then echo  "ERROR! Enabling repos for JBoss EAP Failed" >&2 
 
 echo "Installing JBoss EAP 7.3 repos" | log; flag=${PIPESTATUS[0]}
 echo "yum groupinstall -y jboss-eap7" | log; flag=${PIPESTATUS[0]}
-yum groupinstall -y jboss-eap7        | log; flag=${PIPESTATUS[0]}
-if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP installation Failed" >&2 log; exit $flag;  fi
+yum groupinstall -y jboss-eap7 | log; flag=${PIPESTATUS[0]}
+if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP installation Failed" >&2 log; flag=${PIPESTATUS[0]}exit $flag;  fi
 
 echo "sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config" | log; flag=${PIPESTATUS[0]}
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config | log; flag=${PIPESTATUS[0]}
@@ -144,17 +135,16 @@ echo -e "\t public:inet-address"        | log; flag=${PIPESTATUS[0]}
 
 sudo -u jboss $EAP_HOME/wildfly/bin/jboss-cli.sh --echo-command \
 'embed-server --std-out=echo  --server-config=standalone-azure-ha.xml',\
-'/subsystem=jgroups/channel=ee:write-attribute(name="stack", value="tcp")',\
-'/interface=public:write-attribute(name=inet-address, value="${jboss.bind.address:0.0.0.0}")' | log; flag=${PIPESTATUS[0]}
-
+'/subsystem=jgroups/channel=ee:write-attribute(name="stack", value="tcp")' | log; flag=${PIPESTATUS[0]}
 ####################### Configure the JBoss server and setup eap service
 echo "Setting configurations in $EAP_RPM_CONF_STANDALONE"
 echo -e "\t-> WILDFLY_SERVER_CONFIG=standalone-azure-ha.xml" | log; flag=${PIPESTATUS[0]}
 echo 'WILDFLY_SERVER_CONFIG=standalone-azure-ha.xml' >> $EAP_RPM_CONF_STANDALONE | log; flag=${PIPESTATUS[0]}
 
 echo "Setting configurations in $EAP_LAUNCH_CONFIG"
-echo -e "\t-> JAVA_OPTS=$JAVA_OPTS -Djboss.bind.address=0.0.0.0" | log; flag=${PIPESTATUS[0]}
-echo -e "\t-> JAVA_OPTS=$JAVA_OPTS -Djboss.bind.address.management=0.0.0.0" | log; flag=${PIPESTATUS[0]}
+echo -e '\t-> JAVA_OPTS=$JAVA_OPTS -Djboss.bind.address=0.0.0.0' | log; flag=${PIPESTATUS[0]}
+echo -e '\t-> JAVA_OPTS=$JAVA_OPTS -Djboss.bind.address.management=0.0.0.0' | log; flag=${PIPESTATUS[0]}
+echo -e '\t-> JAVA_OPTS="$JAVA_OPTS -Djboss.bind.address.private=$(hostname -I)"' | log; flag=${PIPESTATUS[0]}
 echo -e '\t-> JAVA_OPTS="$JAVA_OPTS -Djboss.bind.address.private=$(hostname -I)"' | log; flag=${PIPESTATUS[0]}
 
 echo -e 'JAVA_OPTS="$JAVA_OPTS -Djboss.bind.address=0.0.0.0"' >> $EAP_LAUNCH_CONFIG | log; flag=${PIPESTATUS[0]}
@@ -169,26 +159,29 @@ echo -e "JAVA_OPTS=\"\$JAVA_OPTS -Djboss.jgroups.azure_ping.container=$CONTAINER
 echo "Start JBoss-EAP service"                  | log; flag=${PIPESTATUS[0]}
 echo "systemctl enable eap7-standalone.service" | log; flag=${PIPESTATUS[0]}
 systemctl enable eap7-standalone.service        | log; flag=${PIPESTATUS[0]}
+####################### 
 
+###################### Editing eap7-standalone.services
 echo "Adding - After=syslog.target network.target NetworkManager-wait-online.service" | log; flag=${PIPESTATUS[0]}
-sed -i 's/After=syslog.target network.target/After=syslog.target network.target NetworkManager-wait-online.service/' /etc/systemd/system/multi-user.target.wants/eap7-standalone.service
+sed -i 's/After=syslog.target network.target/After=syslog.target network.target NetworkManager-wait-online.service/' /usr/lib/systemd/system/eap7-standalone.service
 echo "Adding - Wants=NetworkManager-wait-online.service \nBefore=httpd.service" | log; flag=${PIPESTATUS[0]}
-sed -i 's/Before=httpd.service/Wants=NetworkManager-wait-online.service \nBefore=httpd.service/' /etc/systemd/system/multi-user.target.wants/eap7-standalone.service
+sed -i 's/Before=httpd.service/Wants=NetworkManager-wait-online.service \nBefore=httpd.service/' /usr/lib/systemd/system/eap7-standalone.service
 echo "systemctl daemon-reload" | log; flag=${PIPESTATUS[0]}
 systemctl daemon-reload
+
 
 echo "systemctl restart eap7-standalone.service"| log; flag=${PIPESTATUS[0]}
 systemctl restart eap7-standalone.service       | log; flag=${PIPESTATUS[0]}
 echo "systemctl status eap7-standalone.service" | log; flag=${PIPESTATUS[0]}
 systemctl status eap7-standalone.service        | log; flag=${PIPESTATUS[0]}
-####################### 
+######################
 
 echo "Deploy an application" | log; flag=${PIPESTATUS[0]}
 echo "wget -O eap-session-replication.war $fileUrl" | log; flag=${PIPESTATUS[0]}
 wget -O "eap-session-replication.war" "$fileUrl" | log; flag=${PIPESTATUS[0]}
 if [ $flag != 0 ] ; then echo  "ERROR! Sample Application Download Failed" >&2 log; exit $flag; fi
 echo "cp ./eap-session-replication.war $EAP_HOME/wildfly/standalone/deployments/" | log; flag=${PIPESTATUS[0]}
-cp ./eap-session-replication.war $EAP_HOME/wildfly/standalone/deployments/ | log; flag=${PIPESTATUS[0]}
+cp ./eap-session-replication.war $EAP_HOME/wildfly/standalone/deployments/ | log; flag=${PIPESTATUS[0]} 
 echo "touch $EAP_HOME/wildfly/standalone/deployments/eap-session-replication.war.dodeploy" | log; flag=${PIPESTATUS[0]}
 touch $EAP_HOME/wildfly/standalone/deployments/eap-session-replication.war.dodeploy | log; flag=${PIPESTATUS[0]}
 
@@ -201,4 +194,4 @@ if [ $flag != 0 ] ; then echo  "ERROR! JBoss EAP management user configuration F
 sleep 20
 
 echo "Red Hat JBoss EAP Cluster Intallation End " | log; flag=${PIPESTATUS[0]}
-/bin/date +%H:%M:%S  | log
+/bin/date +%H:%M:%S  | log; flag=${PIPESTATUS[0]}
