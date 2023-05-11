4440fe7cf39b250f27d986f4e866eee11c8adcad
Adarsh Anand <adarshanshu7@gmail.com>
2023-05-11 14:47:09 +0530

    Add files via upload

---
+++ linux_scripts/docker.sh
@@ -0,0 +1,5 @@
+echo "alias dockps='docker ps --format "{{.ID}}  {{.Names}}"'" >> ~/.bashrc
+echo "alias dcbuild='docker-compose build'" >> ~/.bashrc
+echo "alias dcup='docker-compose up'" >> ~/.bashrc
+echo "alias dcdown='docker-compose down'" >> ~/.bashrc
+echo "alias docksh <id>='docker exec -it <id> /bin/bash'" >> ~/.bashrc
