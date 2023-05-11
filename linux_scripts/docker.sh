2fc631f7c2f830b86e291216a9b46797a124a72d
Adarsh Anand <adarshanshu7@gmail.com>
2023-05-11 15:53:54 +0530

    Update docker.sh

--- linux_scripts/docker.sh
+++ linux_scripts/docker.sh
@@ -1,5 +1,11 @@
-echo "alias dockps='docker ps --format "{{.ID}}  {{.Names}}"'" >> ~/.bashrc
+#!/bin/bash
+
+#Add these lines
+echo "alias dockps='docker ps --format {{.ID}} {{.Names}}'" >> ~/.bashrc
 echo "alias dcbuild='docker-compose build'" >> ~/.bashrc
 echo "alias dcup='docker-compose up'" >> ~/.bashrc
 echo "alias dcdown='docker-compose down'" >> ~/.bashrc
 echo "alias docksh <id>='docker exec -it <id> /bin/bash'" >> ~/.bashrc
+
+# reload bash file
+source ~/.bashrc
