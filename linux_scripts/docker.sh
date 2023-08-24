3c6eec264286e71334e0769321fe80650163ac37
Adarsh Anand <adarshanshu7@gmail.com>
2023-08-24 19:19:46 +0530

    Update docker.sh

--- linux_scripts/docker.sh
+++ linux_scripts/docker.sh
@@ -1,7 +1,10 @@
-HOME="/home/azureuser"
+HOME="/home/demouser"
+PROMPT_DIRTRIM=1
+PATH=$PATH:.
 
+echo "alias ll='ls -l'" >> "$HOME/.bashrc"
 echo "alias dockps='docker ps --format {{.ID}} {{.Names}}'" >> "$HOME/.bashrc"
 echo "alias dcbuild='docker-compose build'" >> "$HOME/.bashrc"
 echo "alias dcup='docker-compose up'" >> "$HOME/.bashrc"
 echo "alias dcdown='docker-compose down'" >> "$HOME/.bashrc"
-echo "alias docksh <id>='docker exec -it <id> /bin/bash'" >> "$HOME/.bashrc"
+echo "docksh() {docker exec -it $1 /bin/bash;}" >> "$HOME/.bashrc"
